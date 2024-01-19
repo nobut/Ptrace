39d66f3f0618545bcdf62eb382ad5878d71dc0d6
lucien <gnepadul@icloud.com>
2024-01-19 22:08:36 +0800

    Update PtraceC.c

--- Sources/PtraceC/PtraceC.c
+++ Sources/PtraceC/PtraceC.c
@@ -4,9 +4,14 @@
 #include "PtraceC.h"
 
 typedef int (*ptrace_ptr_t)(int _request, pid_t _pid, caddr_t _addr, int _data);
+#if !defined(PT_DENY_ATTACH)
 #define PT_DENY_ATTACH 31
+#endif
 
-void disable_gdb() {
-    ptrace_ptr_t ptrace_ptr = dlsym(RTLD_SELF, "ptrace");
-    ptrace_ptr(31, 0, 0, 0); // PTRACE_DENY_ATTACH = 31
+void disable_gdb(){
+    void * handle = dlopen(0, RTLD_GLOBAL | RTLD_NOW);
+    ptrace_ptr_t ptrace_ptr = dlsym(handle, "ptrace");
+    ptrace_ptr(PT_DENY_ATTACH, 0, 0, 0);
+    
+    dlclose(handle);
 }
